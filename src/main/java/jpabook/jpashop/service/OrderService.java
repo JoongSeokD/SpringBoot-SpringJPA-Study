ac7bb2ac610982571474e11ed29e201f701f9a8f
JoongSeokD <ljseokd@gmail.com>
2020-06-01 10:27:44 +0900

    주문 기능 테스트

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -50,6 +50,7 @@ public class OrderService {
     }
 
     //취소
+    @Transactional
     public void cancelOrder(Long orderId){
         Order order = orderRepository.findOne(orderId);
         order.cancel();
